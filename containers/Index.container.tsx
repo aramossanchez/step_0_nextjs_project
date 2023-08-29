9f36d548e31758c2db5699a2c857c694720dfbd0
Armando Ramos Sanchez <armandoramossanchenz@gmail.com>
2023-08-29 18:02:18 +0200

    feat: preparando proyecto de cero

--- containers/Index.container.tsx
+++ containers/Index.container.tsx
@@ -1,8 +1,17 @@
+import Image from 'next/image';
+import { basePath } from '../config/config';
+
 export function IndexContainer() {
 
   return (
     <section className=''>
-      INDEX
+      <Image
+        src={`${basePath}images/logo.png`}
+        width={100}
+        height={100}
+        alt="Logo"
+        className='w-[100px] h-[100px]'
+      />
     </section>
   )
 }
